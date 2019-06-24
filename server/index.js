9b7aeef065563f52895aa2775b497cf086cb1741
Caleb Scoville <calebscoville2010@gmail.com>
2019-06-24 15:44:40 -0600

    index and dotenv

--- server/index.js
+++ server/index.js
@@ -2,18 +2,13 @@ require ('dotenv').config()
 const express = require('express'),
     session = require('express-session'),
     massive = require('massive'),
-    auth_ctrl = require('./controllers/auth_controller')
-    main_ctrl = require('./controllers/main_controller')
-    stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
     bodyParser = require('body-parser')
-    cors = require('cors')
 
 const app = express()
 const { CONNECTION_STRING, SERVER_PORT, SESSION_SECRET } = process.env
 
 app.use(bodyParser.json());
-app.use(cors());
-app.use( express.static( `${__dirname}/../build` ) );
+// app.use( express.static( `${__dirname}/../build` ) );
 
 
 app.use(express.json())
